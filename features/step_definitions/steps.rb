b20c42b6eaa1b4c7a30fa1b0ec9d4dbfce0f029a
saasbook <saasbook@saasbook.(none)>
2015-03-09 01:46:39 -0300

    reverting cucumber

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,5 +1,6 @@
 Given(/^that the following Users exist:$/) do |users_table|
   # table is a Cucumber::Ast::Table
+=begin
   users_table.hashes.each do |user|
     visit path_to(:signup)
     fill_in(:password, :with => user['authentication'])
@@ -9,37 +10,47 @@ Given(/^that the following Users exist:$/) do |users_table|
     click_button("log out")
     visit path_to(:index)
   end
+=end
+  pending
 end
 
 When(/^I am on the home page$/) do
-  visit path_to(:index)
+  #visit path_to(:index)
+  pending
 end
 
 When(/^I am on the log in page$/) do
-  visit path_to(:login)
+  #visit path_to(:login)
+  pending
 end
 
 
 When(/^I fill (.*) with '(.*)'$/) do |field, value|
-  fill_in(field, :with => value)
+  #fill_in(field, :with => value)
+  pending
 end
 
 When(/^I press "(.*?)"$/) do |button|
-  click_button(button)
+  #click_button(button)
+  pending
 end
 
 When(/^I follow "(.*?)"$/) do |link|
-  click_link(link)
+  #click_link(link)
+  pending
 end
 
 Then(/^I should be on the employer home page$/) do
-  assert page.body.contains? "Employer Home Page"
+  #assert page.body.contains? "Employer Home Page"
+  pending
 end
 
 Then(/^I should be on the worker home page$/) do
-  assert page.body.contains? "Worker Home Page"
+  #assert page.body.contains? "Worker Home Page"
+  pending
 end
 
 Then(/^I should be on the login page$/) do
-  assert page.body.contains? "Login Page"
+  #assert page.body.contains? "Login Page"
+  pending
 end
