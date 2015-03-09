f4c4bf82426b0b0df1814f84a52003764cd29851
Drewhaymond <drewhaymond@berkeley.edu>
2015-03-09 16:57:31 -0700

    commenting out facebook auth in user.rb, not sure if needed yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,19 +36,19 @@ class User < ActiveRecord::Base
 
   #->Prelang (user_login/devise)
   has_many :reviews
-  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    user = User.where(provider: auth.provider, uid: auth.uid).first
+  # def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+  #   user = User.where(provider: auth.provider, uid: auth.uid).first
 
-    # The User was found in our database
-    return user if user
+  #   # The User was found in our database
+  #   return user if user
 
-    # The User was not found and we need to create them
-    User.create(name:     auth.extra.raw_info.name,
-                provider: auth.provider,
-                uid:      auth.uid,
-                email:    auth.info.email,
-                password: Devise.friendly_token[0,20])
-  end
+  #   # The User was not found and we need to create them
+  #   User.create(name:     auth.extra.raw_info.name,
+  #               provider: auth.provider,
+  #               uid:      auth.uid,
+  #               email:    auth.info.email,
+  #               password: Devise.friendly_token[0,20])
+  # end
 
 
   attr_accessor :login
