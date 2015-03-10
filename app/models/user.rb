622aecd21312e4334a5157b23749d5647a9a48c4
Amiciate <pengxue@Pengs-MacBook-Pro.local>
2015-03-09 18:12:10 -0700

    commented out find_first_by_auth_conditions function in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,12 +56,12 @@ class User < ActiveRecord::Base
   
   #->Prelang (user_login:devise/username_login_support)
   def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.downcase}]).first
-    else
-      where(conditions).first
-    end
+    # conditions = warden_conditions.dup
+    # if login = conditions.delete(:login)
+    #   where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.downcase}]).first
+    # else
+    #   where(conditions).first
+    # end
   end
 
 
