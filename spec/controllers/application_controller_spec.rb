5b34a688f95c410fd3368791e3726991f03f2276
Amiciate <pengxue@Pengs-MacBook-Pro.local>
2015-03-09 18:03:26 -0700

    added new test

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -5,8 +5,13 @@ describe ApplicationController do
 describe "User not signed in" do
   it "redirects" do
   	visit new_job_path
+  	response.code.should == "302"
+  end
+  it "should not redirect" do
+  	user = FactoryGirl.create(:user)
+    sign_in user
+  	visit new_job_path
   	response.code.should == "200"
   end
-
 end
 end
