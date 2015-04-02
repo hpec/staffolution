4482c8dc3a3d2b4242fcae41b7c070ccae7efd39
Amiciate <pengxue@Pengs-MacBook-Pro.local>
2015-04-01 19:53:26 -0700

    added test for employee

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -41,31 +41,30 @@
 
 FactoryGirl.define do
   factory :employee do
-    employee_email "MyString"
-    employee_phone "MyString"
-    employee_zipcode "MyString"
+    employee_email "MyString@gsndg.com"
+    employee_phone 1234567898
+    employee_zipcode 12345
     employee_first_name "MyString"
     employee_last_name "MyString"
-    user nil
-    employee_bages "MyString"
-    employee_description "MyText"
-    employee_experience_years 1
-    employee_experience_months 1
-    employee_lic_number "MyString"
-    employee_type "MyString"
-    employee_rating 1.5
-    employee_punctuality false
-    employee_dependability false
-    employee_reliable_transportation false
-    employee_accurate_calendar false
-    employee_transportation false
-    employee_school "MyString"
-    employee_languages "MyString"
-    employee_qualifications "MyString"
-    employee_credentials "MyString"
-    employee_experience "MyString"
-    employee_phone_verified false
-    employee_email_verified false
-    employee_lic_verified false
+    # employee_bages "MyString"
+    # employee_description "MyText"
+    # employee_experience_years 1
+    # employee_experience_months 1
+    # employee_lic_number "MyString"
+    # employee_type "MyString"
+    # employee_rating 1.5
+    # employee_punctuality false
+    # employee_dependability false
+    # employee_reliable_transportation false
+    # employee_accurate_calendar false
+    # employee_transportation false
+    # employee_school "MyString"
+    # employee_languages "MyString"
+    # employee_qualifications "MyString"
+    # employee_credentials "MyString"
+    # employee_experience "MyString"
+    # employee_phone_verified false
+    # employee_email_verified false
+    # employee_lic_verified false
   end
 end
