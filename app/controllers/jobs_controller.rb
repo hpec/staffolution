803e907d608b11a98d483a3294a9514ddadd12dd
Daniel Liu <hpec@qq.com>
2015-04-13 01:59:24 -0700

    some fixes

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,29 @@
 class JobsController < InheritedResources::Base
 
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+  before_filter :set_employer
+
+  def new
+    @job = Job.new
+    @job.address_line_1 = @employer.address_line_1
+    @job.address_line_2 = @employer.address_line_2
+    @job.city = @employer.city
+    @job.state = @employer.state
+    @job.zipcode = @employer.zipcode
+  end
+
   private
 
     def job_params
-      params.require(:job).permit(:name, :description, :employer_id, :employee_id, :address_line_1, :address_line_2, :city, :state, :zipcode, :compensation, :position)
+      params.require(:job).permit(:name, :description, :address_line_1, :address_line_2, :city, :state, :zipcode, :compensation, :position)
+    end
+
+    def set_employer
+      if current_user.employer.nil?
+        flash[:error] = "You have to create your employer profile first!"
+        redirect_to root_path
+      end
+      @employer = current_user.employer
     end
 end
 
